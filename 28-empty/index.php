049af8060b2cd66c9f38607ca7a4460c6df8c027
Guilherme Henrique do Amaral <guilhermeha77@outlook.com>
2026-02-06 16:58:35 -0300

    Add 28-empty

---
+++ 28-empty/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+if (empty($variavel)) { } -> Executa se a variável estiver vazia
+Uma variável é considerada vazia se for:
+    * "" (uma string vazia)
+    * 0 (0 como um inteiro)
+    * 0.0 (0 como um float)
+    * "0" (0 como uma string)
+    * null
+    * false
+    * array() (um array vazio)
+    * $var; (uma variável declarada, mas sem valor)
+*/
